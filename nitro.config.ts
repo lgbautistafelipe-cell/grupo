065ab31b4cc086df06df72773497b59a6898a9cc
lgbautistafelipe-cell <lgbautistafelipe@gmail.com>
2026-06-03 18:59:17 -0300

    fix: simplify Nitro config, remove conflicting rollup config

--- nitro.config.ts
+++ nitro.config.ts
@@ -2,11 +2,4 @@ import { defineNitroConfig } from "nitropack/config";
 
 export default defineNitroConfig({
   preset: "vercel",
-  serveStatic: true,
-  noAnalyze: true,
-  rollupConfig: {
-    output: {
-      dir: ".vercel/output/functions/index.func",
-    },
-  },
 });
