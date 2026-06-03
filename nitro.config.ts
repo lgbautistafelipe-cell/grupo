e94ca1edf9df33ec2bdcc29417522140cbd14268
lgbautistafelipe-cell <lgbautistafelipe@gmail.com>
2026-06-03 18:58:31 -0300

    fix: update Nitro config to properly output for Vercel

--- nitro.config.ts
+++ nitro.config.ts
@@ -3,4 +3,10 @@ import { defineNitroConfig } from "nitropack/config";
 export default defineNitroConfig({
   preset: "vercel",
   serveStatic: true,
+  noAnalyze: true,
+  rollupConfig: {
+    output: {
+      dir: ".vercel/output/functions/index.func",
+    },
+  },
 });
